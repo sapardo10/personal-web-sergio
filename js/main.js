1a69c4140db9ffe795ce28aca8f0584e36a9c094
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-03-21 22:45:24 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,102 +1,24 @@
-"use strict";
-$(document).ready(function() {
-	// Header Scroll
-	$(window).on('scroll', function() {
-		var scroll = $(window).scrollTop();
 
-		if (scroll >= 50) {
-			$('#header').addClass('fixed');
-		} else {
-			$('#header').removeClass('fixed');
-		}
+	var language='es';
+
+	function getLanguage() {
+	(localStorage.getItem('language') == null) ? setLanguage('en') : false;
+	$.ajax({
+		url:  '/language/' +  localStorage.getItem('language') + '.json',
+		dataType: 'json',
+		async: false,
+		dataType: 'json',
+		success: function (lang) {
+			 language = lang
+		 }
+	 });
+	}
+
+	function setLanguage(lang) {
+	localStorage.setItem('language', lang);
+	}
+
+	$(document).ready(function(){
+	$('#h1-summary').text(language.titleSummary);
+	console.log(language.titleSummary);
 	});
-
-	// Fancybox
-	$('.work-box').fancybox();
-
-	// Flexslider
-	$('.flexslider').flexslider({
-		animation: "fade",
-		directionNav: false,
-	});
-
-	// Page Scroll
-	var sections = $('section'),
-		nav = $('nav[role="navigation"]');
-
-	$(window).on('scroll', function () {
-	  	var cur_pos = $(this).scrollTop();
-	  	sections.each(function() {
-	    	var top = $(this).offset().top - 76,
-	        	bottom = top + $(this).outerHeight();
-	    	if (cur_pos >= top && cur_pos <= bottom) {
-	      		nav.find('a').removeClass('active');
-	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-	    	}
-	  	});
-	});
-	nav.find('a').on('click', function () {
-	  	var $el = $(this)
-	    	id = $el.attr('href');
-		$('html, body').animate({
-			scrollTop: $(id).offset().top - 75
-		}, 500);
-	  return false;
-	});
-
-	// Mobile Navigation
-	$('.nav-toggle').on('click', function() {
-		$(this).toggleClass('close-nav');
-		nav.toggleClass('open');
-		return false;
-	});	
-	nav.find('a').on('click', function() {
-		$('.nav-toggle').toggleClass('close-nav');
-		nav.toggleClass('open');
-	});
-});
-
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
-})(jQuery);
-
-	var habilidades = $("#habilidad");
-function frontEnd(){
-  	alert("se metio a fron!");
-  	return 
-  };
-  function backEnd(){
-  	alert("se metio a back!");
-  };
-  function arquitecto(){
-  	alert("se metio a arquitecto!");
-  };
-  function seguridad(){
-  	alert("se metio a seguridad!");
-  };
